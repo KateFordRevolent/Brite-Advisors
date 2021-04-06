e57166af62edc5ab9c734cb024d24001bd821e50
Kate Ford <k.ford@revolentgroup.com>
2021-04-06 11:30:59 +0100

    email changes

--- force-app/main/default/lwc/advisorDetails/advisorDetails.js
+++ force-app/main/default/lwc/advisorDetails/advisorDetails.js
@@ -8,16 +8,30 @@ export default class AdvisorDetails extends LightningElement
 {
     // Properties
     userId = Id;
+    @track contact;
+    @track emailBody;
+    @track error;
 
     // Expose the static resource URL for use in the template
     advisorImageStaticUrl = advisorImageStaticUrl;
 
     // Expose URL of assets included inside an archive file
     advisorImagesUrl = advisorImagesUrl + '/advisorImages/AdvisorImagePending.png';
+   
+    contactObjForListener = getAdvisorDetails(userId);
+    //console.log('contactObjForListener:', contactObjForListener);
 
-    @track contact;
-    @track error;
-    @track contactName;
+    // TODO HTML remove when finished debugging email address
+    //<lightning-input data-name="emailAddress" value={contact.Owner.Email}></lightning-input>
+
+    console.log('in exp Id is ', Id);
+    console.log('in exp userId  is ', userId);
+    console.log('in exp advisorImagesUrl is ', advisorImagesUrl);
+    console.log('in exp this.contactObjForListener is ', contactObjForListener);
+    console.log('in exp advisorImageStaticUrl is ', advisorImageStaticUrl);
+     console.log('in exp this.contact is ', contact);
+    console.log('in exp emailBody is ', emailBody);
+    console.log('in exp error is ', error);
   
     @wire(getAdvisorDetails, {recId: '$userId' }) wiredContact({ error, data})
     { 
@@ -25,28 +39,49 @@ export default class AdvisorDetails extends LightningElement
         {
             console.log(data);
             this.contact = data;
-            this.contactName = data.Name; // not needed???
+
+            console.log('in wire Id is ', Id);
+            console.log('in wire userId  is ', userId);
+            console.log('in wire advisorImagesUrl is ', advisorImagesUrl);
+            console.log('in wire this.contactObjForListener is ', contactObjForListener);
+            console.log('in wire advisorImageStaticUrl is ', advisorImageStaticUrl);
+            console.log('in wire this.contact is ', contact);
+            console.log('in wire emailBody is ', emailBody);
+            console.log('in wire error is ', error);
 
         }
         else if (error) 
         {
             this.error = error;
-            console.log(this.error);
+            console.log('in wire error Id is ', Id);
+            console.log('in wire error userId  is ', userId);
+            console.log('in wire error advisorImagesUrl is ', advisorImagesUrl);
+            console.log('in wire error this.contactObjForListener is ', contactObjForListener);
+            console.log('in wire error advisorImageStaticUrl is ', advisorImageStaticUrl);
+            console.log('in wire error this.contact is ', contact);
+            console.log('in wire error emailBody is ', emailBody);
+            console.log('in wire error error is ', error);
+            
         }
     }
 
-    //emailAddress; 
-    //<lightning-input data-name="emailAddress" value={contact.Owner.Email}></lightning-input>
-    
-    @track emailBody;
-
+        
     handleChange(event) {
         if (event.target.name === 'emailBody') {
 
-            console.log('event target name', event.target.name);
             this.emailBody = event.target.value;
 
+            //TODO remove when finished debuggin email address
             //this.emailAddress = this.template.querySelector("[data-name = 'emailAddress']").value;
+
+            // console.log('in handlerchange Id is ', Id);
+            // console.log('in handlerchange userId  is ', userId);
+            // console.log('in handlerchange advisorImagesUrl is ', advisorImagesUrl);
+            // console.log('in handlerchange this.contactObjForListener is ', contactObjForListener);
+            // console.log('in handlerchange advisorImageStaticUrl is ', advisorImageStaticUrl);
+            // console.log('in handlerchange this.contact is ', contact);
+            // console.log('in handlerchange emailBody is ', emailBody);
+            // console.log('in handlerchange error is ', error);
             
         }
     }
@@ -55,8 +90,8 @@ export default class AdvisorDetails extends LightningElement
         // send mail
         sendEmail({ toAddress: 'briteadvisorsmvp@gmail.com', subject: 'Attention: Brite Advisor Client Portal Message!', body: this.emailBody});
         
-        //console.log('emailAddress is',this.emailAddress);
-
-        console.log('emailBody is ',this.emailBody);
+        //console.log('in seh, this.contactObjForListener is',this.contactObjForListener);
+        //console.log('in seh, this.contact is',this.contact);
+        //console.log('in seh, emailBody is ',this.emailBody);
     }
 }
